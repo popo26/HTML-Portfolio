f1cb0591421c06f7794858d8a2b5773e423cf50d
popo26 <mynameispopo26@gmail.com>
2023-08-10 15:10:44 +1200

    in progress

---
+++ js/greeting.js
@@ -0,0 +1,27 @@
+const greeting1 = document.querySelector('.greeting1');
+const greeting2 = document.querySelector('.greeting2');
+
+
+setTimeout(() => {
+    greeting1.style.visibility = 'visible';
+    greeting1.innerHTML = "Hi! I'm Ai!";
+}, 2000);
+
+
+setTimeout(() => {
+    greeting2.style.visibility = 'visible';
+    greeting2.innerHTML = "A Junior Developer";
+}, 4000);
+
+setTimeout(() => {
+    greeting1.style.visibility = 'hidden';
+   
+}, 10000);
+
+setTimeout(() => {
+    greeting2.style.visibility = 'hidden'; 
+}, 12000);
+
+
+
+
